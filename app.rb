df0fca3ac1e537d7110d4f93c983ff3283283cf8
Nicole Javorsky <nsj2110@barnard.edu>
2015-05-23 19:47:47 -0400

    added sinatra files

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+
+module nicolieolie
+  class Application < Sinatra::Base
+
+    configure do
+      set :root, File.dirname(__FILE__)
+      set :logger, $logger
+
+      set :public_folder, 'public'
+
+      set :raise_errors, false
+      set :show_exceptions, false
+    end
+
+    not_found do
+      erb :error
+    end
+
+    error 400..510 do
+      erb :error
+    end
+
+    get '/' do
+      erb :home
+    end
+
+  end
+end
